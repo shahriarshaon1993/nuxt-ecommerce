b0b465507ab42e7b1eb099f1ed589d0e6f96a49b
shahriarshaon1993 <shahriarshaon1993@gmail.com>
2022-10-12 01:34:29 +0600

    modify nuxt.config.js file

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,12 +9,11 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - ecommerce',
     title: 'ecommerce',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      { hid: 'description', name: 'description', content: 'Ecommerce Website' },
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
@@ -23,8 +22,7 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
+  css: ["@sweetalert2/theme-material-ui", "~/assets/main.css"],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
@@ -45,7 +43,15 @@ export default {
     '@nuxtjs/pwa',
     // https://go.nuxtjs.dev/content
     '@nuxt/content',
+    'vue-sweetalert2/nuxt/no-css',
+    'nuxt-webfontloader',
   ],
+
+  webfontloader: {
+    google: {
+      families: ["DM+Sans:wght@400;500;700&display=swap"],
+    },
+  },
 
   // PWA module configuration: https://go.nuxtjs.dev/pwa
   pwa: {
@@ -60,18 +66,38 @@ export default {
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
+    treeShake: true,
+    defaultAssets: {
+      font: {
+        family: "DM Sans",
+        size: 15,
+      },
+    },
     theme: {
-      dark: true,
+      light: true,
       themes: {
-        dark: {
-          primary: colors.blue.darken2,
+        light: {
+          primary: colors.deepPurple.darken1,
+          bg: "#ffffff",
+          surface: "#ffffff",
           accent: colors.grey.darken3,
           secondary: colors.amber.darken3,
           info: colors.teal.lighten1,
           warning: colors.amber.base,
           error: colors.deepOrange.accent4,
-          success: colors.green.accent3
-        }
+          success: colors.green.accent3,
+        },
+        dark: {
+          primary: colors.deepPurple.darken1,
+          bg: "#0a0514",
+          surface: "#130a24",
+          accent: colors.grey.darken3,
+          secondary: colors.amber.darken3,
+          info: colors.teal.lighten1,
+          warning: colors.amber.base,
+          error: colors.deepOrange.accent4,
+          success: colors.green.accent3,
+        },
       }
     }
   },
