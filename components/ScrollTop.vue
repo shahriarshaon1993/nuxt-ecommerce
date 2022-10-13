45235a2e4baa075b1f4967b9b48c16e38d88ef8c
shahriarshaon1993 <shahriarshaon1993@gmail.com>
2022-10-13 16:18:14 +0600

    Home page done

---
+++ components/ScrollTop.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-fab-transition>
+    <v-btn
+      fixed
+      right
+      bottom
+      fab
+      @click="goUp"
+      color="primary"
+      v-scroll="checkScroll"
+      v-show="showFab"
+    >
+      <v-icon>mdi-chevron-up</v-icon>
+    </v-btn>
+  </v-fab-transition>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      showFab: false,
+    };
+  },
+  methods: {
+    checkScroll() {
+      const height = window.scrollY;
+
+      if (height > 300) {
+        this.showFab = true;
+      } else {
+        this.showFab = false;
+      }
+    },
+
+    goUp() {
+      this.$vuetify.goTo(0, {
+        duration: 1000,
+      });
+    },
+  },
+};
+</script>
+
+<style>
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,42 @@
 <template>
   <div>
     <Nav />
-
+    <HomeCarousel :sale_items="sale_items"/>
+    <br><br>
+    <v-container>
+      <h1 class="text-md-h4 text-h6">Check these out</h1>
+      <br>
+      <ProductSlider :products="products"/>
+      <br><br><br>
+      <Newsletter/>
+    </v-container>
     <Footer />
+
+    <ScrollTop />
   </div>
 </template>
 
 <script>
 import Nav from "~/components/Nav.vue";
 import Footer from "~/components/Footer.vue";
-export default { components: { Nav, Footer } };
+import HomeCarousel from "~/components/HomeCarousel.vue";
+import ProductSlider from "~/components/ProductSlider.vue";
+import Newsletter from "~/components/Newsletter.vue";
+import ScrollTop from "~/components/ScrollTop.vue";
+
+export default {
+  components: { Nav, Footer, HomeCarousel, ProductSlider, Newsletter, ScrollTop },
+  data() {
+    return {
+      products: null,
+      sale_items: null
+    }
+  },
+  async created() {
+    this.sale_items = await this.$content('products').where({onSale: true}).fetch();
+    this.products = await this.$content('products').fetch();
+  }
+};
 </script>
 
 <style></style>
